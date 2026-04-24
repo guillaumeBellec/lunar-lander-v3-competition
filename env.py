f3288894c38256f711eac0a30d6308c504567529
Guillaume Bellec <guillaume.bellec.eu@gmail.com>
2026-04-24 11:01:11 +0200

    base

---
+++ env.py
@@ -0,0 +1,13 @@
+import gymnasium as gym
+
+ENV_KWARGS = dict(
+    continuous=False,
+    gravity=-7.5,
+    enable_wind=True,
+    wind_power=12.0,
+    turbulence_power=1.2,
+)
+
+
+def make_env(render_mode=None):
+    return gym.make("LunarLander-v3", render_mode=render_mode, **ENV_KWARGS)
